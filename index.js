523e2350d8816bc52d0fd2abd2b945a46f62290f
narip <narip@github.com>
2024-02-04 21:59:57 +0900

    fix: react, emotion, 기타 react 라이브러리 순으로 import order 세팅

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ module.exports = {
       version: 'detect',
     },
     'import/resolver': {
-      typescript: {
-      },
+      typescript: {},
       node: {
         extensions: ['.ts', '.tsx', '.js', '.jsx'],
       },
@@ -32,9 +31,9 @@ module.exports = {
   rules: {
     /** prettier 에러 */
     'prettier/prettier': 'error',
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     /** emotion/react 의 css 사용시 에러 무시 */
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/no-unknown-property': ['error', {ignore: ['css']}],
     /** JSX 안에서 HTML 엔티티(escape sequence)를 사용하면 에러 */
     'react/no-unescaped-entities': ['error', {
       forbid: [{
@@ -52,13 +51,13 @@ module.exports = {
     /** JSX 존재한다면 React를 import하도록 강제하는 규칙 */
     'react/react-in-jsx-scope': 'off',
     /** 무의미한 Fragment 를 막는 규칙 <>{foo}</> 이건 가능 */
-    'react/jsx-no-useless-fragment': ['off', { allowExpressions: true }],
+    'react/jsx-no-useless-fragment': ['off', {allowExpressions: true}],
     /** JSX 네이밍 규칙 강제 */
-    'react/jsx-pascal-case': ['error', { allowAllCaps: false, allowNamespace: true, allowLeadingUnderscore: false }],
+    'react/jsx-pascal-case': ['error', {allowAllCaps: false, allowNamespace: true, allowLeadingUnderscore: false}],
     /** JSX props 불필요한 스페이싱을 막는 규칙 */
     'react/jsx-props-no-multi-spaces': 'error',
     /** jsx를 사용했는데 확장자가 .jsx가 아니면 에러를 내는 규칙*/
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/jsx-filename-extension': ['error', {extensions: ['.tsx', '.jsx']}],
     /** 셀프 클로징 가능한 상태일 때 셀프 클로징을 강제하는 규칙 */
     'react/self-closing-comp': ['error', {
       'component': true,
@@ -84,56 +83,70 @@ module.exports = {
     /** 정의되지 않은 변수 사용 방지 규칙 */
     'no-undef': 'error',
     /** 변수 및 함수를 정의 전에 사용할 수 있게 해주는 규칙 */
-    'no-use-before-define': ['error', { variables: false, functions: false }],
+    'no-use-before-define': ['error', {variables: false, functions: false}],
     /** foo['bar'] 대신 foo.bar 사용 강제 규칙 */
     'dot-notation': 'error',
     /** 한 줄당 글자 제한하는 규칙, 주석 제외 */
-    'max-len': ['error', { code: 100, ignoreComments: true, ignoreStrings: true }],
+    'max-len': ['error', {code: 100, ignoreComments: true, ignoreStrings: true}],
     /** dependencies인 모듈이 devDependencies에 있는 경우 에러 발생시키는 규칙 */
     "import/no-extraneous-dependencies": ['error', {"devDependencies": true}],
     /** import 시에 파일의 확장자를 쓰지 않도록 강제하는 규칙 */
-    'import/extensions': ['error', { json: 'always' }],
+    'import/extensions': ['error', {json: 'always'}],
     'import/no-unresolved': 'error',
     /** import 구문 중괄호 사이의 member 모듈 알파벳 순으로 정렬하는 규칙 */
-    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'sort-imports': ['error', {ignoreDeclarationSort: true}],
     'unused-imports/no-unused-vars': [
       'error',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      {vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_'},
     ],
     /** import 순서 */
-    'import/order': ['error', {
-      warnOnUnassignedImports: true,
-      pathGroupsExcludedImportTypes: ['type'],
-      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
-      pathGroups: [
-        {
-          pattern: 'react*',
-          group: 'external',
-          position: 'before',
+    'import/order': [
+      'error',
+      {
+        warnOnUnassignedImports: true,
+        pathGroupsExcludedImportTypes: ['type'],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@emotion/*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '{react*,*/react}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'lodash-es',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '**/*.+(css|sass|less|scss|pcss|style|svg)',
+            patternOptions: {dot: true, nocomment: true},
+            group: 'unknown',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
-        {
-          pattern: 'lodash-es',
-          group: 'external',
-          position: 'before',
-        },
-        {
-          pattern: '@/**',
-          group: 'internal',
-          position: 'before',
-        },
-        {
-          pattern: '**/*.+(css|sass|less|scss|pcss|style|svg)',
-          patternOptions: { dot: true, nocomment: true },
-          group: 'unknown',
-          position: 'after',
-        }],
-      alphabetize: {
-        order: 'asc',
-        caseInsensitive: true,
+        /** import 구문 개행 금지 */
+        'newlines-between': 'never',
       },
-      /** import 구문 개행 금지 */
-      'newlines-between': 'never',
-    }],
+    ],
   },
   overrides: [{
     files: ['**/*.ts', '**/*.tsx'],
@@ -152,6 +165,6 @@ module.exports = {
     "files": ["test/**", "**.test.ts", "**.test.tsx"],
     "plugins": ["jest"],
     "extends": ["plugin:jest/recommended"],
-    "rules": { "jest/prefer-expect-assertions": "off" }
+    "rules": {"jest/prefer-expect-assertions": "off"}
   }]
 };
